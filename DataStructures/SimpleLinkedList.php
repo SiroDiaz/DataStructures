df2f2a16dad57ed23ba8bc39731aae6ec7dc70be
Siro Díaz Palazón <sirodiaz93@gmail.com>
2017-05-26 21:48:20 +0200

    Added more methods to simple linked list and tests

--- DataStructures/SimpleLinkedList.php
+++ DataStructures/SimpleLinkedList.php
@@ -1,7 +1,81 @@
 <?php
 
-use DataStructures;
+namespace DataStructures;
+
+class SimpleLinkedListNode {
+    public $data = null;
+    public $next = null;
+
+    public function __construct($data) {
+        $this->data = $data;
+    }
+}
 
 class SimpleLinkedList {
-    
+    private $head;
+    private $size;
+
+    public function __construct() {
+        $this->head = null;
+        $this->size = 0;
+    }
+
+    public function size() : int {
+        return $this->size;
+    }
+
+    public function empty() : bool {
+        return $this->size == 0;
+    }
+
+    public function push($node) {
+        $newNode = new SimpleLinkedListNode($node);
+        if($this->size === 0) {
+            $this->head = &$newNode;
+        } else {
+            $current = $this->head;
+            while($current->next !== null) {
+                $current = $current->next;
+            }
+            $current->next = &$newNode;
+        }
+
+        $this->size++;
+    }
+
+    public function get($index) {
+        if($this->head === null || $index > $this->size - 1 || $index < 0) {
+            return null;
+        }
+        
+
+        $current = $this->head;
+        $i = 0;
+        while($i < $index && $current->next !== null) {
+            $current = $current->next;
+            $i++;
+        }
+
+        return $current->data;
+    }
+
+    public function getAll() {
+        if($this->head === null) {
+            return null;
+        }
+
+        $current = $this->head;
+        while($current->next !== null) {
+            yield $current->data;
+            $current = $current->next;
+        }
+    }
+
+    public function insertAt($node) {
+
+    }
+
+    public function unshift($node) {
+
+    }
 }
--- tests/SimpleLinkedListTest.php
+++ tests/SimpleLinkedListTest.php
@@ -1,12 +1,30 @@
 <?php
 
-
+use DataStructures\SimpleLinkedList;
 use PHPUnit\Framework\TestCase;
 
 class SimpleLinkedListTest extends TestCase {
-    
+    private $list;
+
+    public function setUp() {
+        $this->list = new SimpleLinkedList();
+    }
+
+    public function testSize() {
+        $this->assertEquals($this->list->size(), 0);
+    }
+
+    public function testEmpty() {
+        $this->assertTrue($this->list->empty());
+    }
+
     public function testPush() {
-        $this->assertEquals(true, true);
+        $this->list->push(20);
+        $this->assertEquals(1, $this->list->size());
+        $this->assertEquals(20, $this->list->get(0));
+        $this->list->push(true);
+        $this->assertEquals(2, $this->list->size());
+        $this->assertTrue($this->list->get(1));
     }
 
     public function testInsertAt() {
@@ -15,5 +33,13 @@ class SimpleLinkedListTest extends TestCase {
 
     public function testUnshift() {
         $this->assertEquals(true, true);
+    }
+
+    public function testPop() {
+        $this->assertTrue(true);
+    }
+
+    public function testShift() {
+        $this->assertTrue(true);
     }
 }
