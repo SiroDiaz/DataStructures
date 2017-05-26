10054fc7aa3f084a90bd28ac35ba1c0411990e5b
Siro Díaz Palazón <sirodiaz93@gmail.com>
2017-05-26 18:22:18 +0200

    Initial tests and simple linked list

---
+++ tests/SimpleLinkedListTest.php
@@ -0,0 +1,19 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleLinkedListTest extends TestCase {
+    
+    public function testPush() {
+        $this->assertEquals(true, true);
+    }
+
+    public function testInsertAt() {
+        $this->assertEquals(true, true);
+    }
+
+    public function testUnshift() {
+        $this->assertEquals(true, true);
+    }
+}
