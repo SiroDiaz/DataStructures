13d9a5020673d49d2654925e51f24672dfd2e038
Siro Díaz Palazón <sirodiaz93@gmail.com>
2017-05-30 11:19:21 +0200

    starting queues

--- DataStructures/Lists/Queue.php
+++ DataStructures/Lists/Queue.php
@@ -2,19 +2,48 @@
 
 namespace DataStructures\Lists;
 
+/**
+ * Queue is a circular linked list that inserts at the end
+ * of list and removes at the beginning. Insert and remove
+ * are O(1), size and empty are also O(1).
+ *
+ * @author Siro Diaz Palazon <siro_diaz@yahoo.com>
+ */
 class Queue {
+    private $head;
+    private $tail;
+    private $size;
+
+    public function __construct() {
+        $this->head = null;
+        $this->tail = &$this->head;
+        $this->size = 0;
+    }
+
+    /**
+     *
+     */
     public function size() : int {
         return 0;
     }
 
+    /**
+     *
+     */
     public function empty() : boolean {
         return false;
     }
 
+    /**
+     *
+     */
     public function enqueue($data) {
 
     }
     
+    /**
+     *
+     */
     public function dequeue() {
 
     }
---
+++ tests/Lists/QueueTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use DataStructures\Lists\Queue;
+use PHPUnit\Framework\TestCase;
+
+class QueueTest extends TestCase {
+    private $queue;
+
+    public function setUp() {
+        $this->queue = new Queue();
+    }
+
+    public function testEnqueue() {
+
+    }
+
+    public function testDequeue() {
+
+    }
+}
