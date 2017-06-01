3724bc7f95f8c08227ff5a596ce7db3f95ad58d4
Siro Díaz Palazón <sirodiaz93@gmail.com>
2017-06-01 14:45:05 +0200

    description about BST

--- DataStructures/Trees/BinarySearchTree.php
+++ DataStructures/Trees/BinarySearchTree.php
@@ -4,6 +4,13 @@ namespace DataStructures\Trees;
 
 use DataStructures\Trees\Interfaces\TreeInterface;
 
+/**
+ * BinarySearchTree class. Represents a BST actions that can be realized.
+ * At the beginning root is null and it can grow up to a O(n) in search,
+ * delete and insert (in worst case). In best cases it will be O(log n).
+ *
+ * @author Siro Diaz Palazon <siro_diaz@yahoo.com>
+ */
 class BinarySearchTree implements TreeInterface {
     private $root;
     private $size;
