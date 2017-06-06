f3b505bffa4572493aff988c706bc29db0ec788e
Siro Díaz Palazón <sirodiaz93@gmail.com>
2017-06-06 13:38:58 +0200

    added ArrayList class

---
+++ DataStructures/Lists/ArrayList.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace DataStructures\Lists;
+
+use DataStructures\Lists\Interfaces\ListInterface;
+
+class ArrayList implements ListInterface {
+    public function insert($index, $data) {
+
+    }
+    
+    public function clear() {
+
+    }
+    
+    public function get($index) {
+
+    }
+
+    public function getAll() {
+
+    }
+
+    public function empty() : bool {
+        return true;
+    }
+    
+    public function delete($index) {
+
+    }
+    
+    public function size() : int {
+        return 0;
+    }
+
+    public function toArray() : array {
+        return [];
+    }
+}
