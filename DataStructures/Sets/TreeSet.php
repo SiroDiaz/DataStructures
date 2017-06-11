36312e5fd2f640176e140ef3e5c8c4b9cbf2ccb3
Siro Díaz Palazón <sirodiaz93@gmail.com>
2017-06-12 00:30:33 +0200

    Modified children to a BST

---
+++ DataStructures/Sets/TreeSet.php
@@ -0,0 +1,3 @@
+<?php
+
+// spl_object_hash usage for get the hash and be used to store. (no sort by the moment)
--- DataStructures/Trees/Nodes/TrieNode.php
+++ DataStructures/Trees/Nodes/TrieNode.php
@@ -1,7 +1,23 @@
 <?php
-
+/**
+ * DataStructures for PHP
+ *
+ * @link      https://github.com/SiroDiaz/DataStructures
+ * @copyright Copyright (c) 2017 Siro Díaz Palazón
+ * @license   https://github.com/SiroDiaz/DataStructures/blob/master/README.md (MIT License)
+ */
 namespace DataStructures\Trees;
 
+use DataStructures\Trees\BinarySearchTree;
+
+/**
+ * TrieNode.
+ *
+ * The TrieNode class represents the trie node. It uses a BST to store all children
+ * nodes in worst case of O(n).
+ *
+ * @author Siro Diaz Palazon <siro_diaz@yahoo.com>
+ */
 class TrieNode {
     public $char;
     public $isWord;
@@ -11,7 +27,7 @@ class TrieNode {
     public function __construct($char = '', $isWord = false) {
         $this->char = $char;
         $this->isWord = $isWord;
-        $this->children = [];
+        $this->children = new BinarySearchTree();
         $this->size = 0;
     }
 }
