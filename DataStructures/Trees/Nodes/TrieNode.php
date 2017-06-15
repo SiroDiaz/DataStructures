738d970a43bc60e56cf750bc8f8b8abfed19bc61
Siro Díaz Palazón <sirodiaz93@gmail.com>
2017-06-15 10:10:25 +0200

    documentation for trie node

--- DataStructures/Trees/Nodes/TrieNode.php
+++ DataStructures/Trees/Nodes/TrieNode.php
@@ -31,18 +31,39 @@ class TrieNode implements Countable {
         $this->parent = &$parent;
     }
 
+    /**
+     * Returns if the node has children.
+     *
+     * @return bool true if there is one or more children.
+     */
     public function hasChildren() : bool {
         return count($this->children) > 0;
     }
 
+    /**
+     * Returns if the node is leaf: it has not children nodes and
+     * it is not root.
+     *
+     * @return bool true if is leaf.
+     */
     public function isLeaf() : bool {
         return $this->parent !== null && $this->hasChildren() === false;
     }
 
+    /**
+     * Checks if is the node is root.
+     *
+     * @return bool true is it has not parent node.
+     */
     public function isRoot() : bool {
         return $this->parent === null;
     }
 
+    /**
+     * Returns the number of children. Binds count().
+     *
+     * @return int The node count.
+     */
     public function count() {
         return count($this->children);
     }
