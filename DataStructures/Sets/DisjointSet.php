42e8583067140fa1b57f0e577d20f851278306a2
Siro Díaz Palazón <sirodiaz93@gmail.com>
2017-06-21 19:18:32 +0200

    added union operation and refactor in DisjointNode

--- DataStructures/Sets/DisjointSet.php
+++ DataStructures/Sets/DisjointSet.php
@@ -7,8 +7,8 @@ class DisjointNode {
     public $rank;
     public $data;
 
-    public function __construct(DisjointNode $parent = null, $rank, $data) {
-        $this->parent = $parent;
+    public function __construct($rank, $data) {
+        $this->parent = null;
         $this->rank = $rank;
         $this->data = $data;
     }
@@ -18,13 +18,13 @@ class DisjointSet {
     public function __construct() {}
 
     public function makeSet($data) {
-        $newSet = new DisjointNode(null, 0, $data);
+        $newSet = new DisjointNode(0, $data);
         $newSet->parent = &$newSet;
 
         return $newSet;
     }
 
-    public function find(DisjointNode $node) {
+    public function find(DisjointNode $node) : DisjointNode {
         if($node->parent !== $node) {
             $node->parent = $this->find($node->parent);
         }
@@ -33,6 +33,20 @@ class DisjointSet {
     }
 
     public function union($x, $y) {
-        
+        $rootX = $this->find($x);
+        $rootY = $this->find($y);
+
+        if($rootX === $rootY) {
+            return;
+        }
+
+        if($rootX->rank < $rootY->rank) {
+            $rootX->parent = &$rootY;
+        } else if($rootX->rank > $rootY->rank) {
+            $rootY->parent = &$rootX;
+        } else {
+            $rootX->parent = &$rootY;
+            $rootY->rank++;
+        }
     }
 }
--- tests/Sets/DisjointSetTest.php
+++ tests/Sets/DisjointSetTest.php
@@ -14,16 +14,20 @@ class DisjointSetTest extends TestCase {
         $sets = [];
         $sets[] = $this->set->makeSet('hello');
         $sets[] = $this->set->makeSet('goodbye');
-        $this->assertEquals(true, true);
+        $this->assertEquals($sets[0], $this->set->makeSet('hello'));
+        $this->assertEquals($sets[1], $this->set->makeSet('goodbye'));
     }
 
-    /*
-    public function testFindSet() {
-        $this->set->makeSet('hello');
-        $this->set->makeSet('goodbye');
-        $this->set->union(0, 1);
-        var_dump($this->set);
-        $this->assertEquals(2, count($this->set));
+    public function testUnion() {
+        $sets = [];
+        $sets[] = $this->set->makeSet('hello');
+        $sets[] = $this->set->makeSet('goodbye');
+        $sets[] = $this->set->makeSet([true, 3.14]);
+
+        $this->set->union($sets[0], $sets[1]);
+        $this->set->union($sets[0], $sets[0]);
+        $this->assertEquals($sets[1], $this->set->find($sets[0]));
+        $this->set->union($sets[0], $sets[2]);
+        $this->assertEquals($sets[1], $this->set->find($sets[2]));
     }
-    */
 }
