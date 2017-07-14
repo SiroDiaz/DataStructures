b45929c4e42617c74e8b8d1af5351bd78fad191e
Siro Díaz Palazón <sirodiaz93@gmail.com>
2017-07-15 00:19:56 +0200

    Added NotFoundException

---
+++ DataStructures/Exceptions/NotFoundException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * DataStructures for PHP
+ *
+ * @link      https://github.com/SiroDiaz/DataStructures
+ * @copyright Copyright (c) 2017 Siro Díaz Palazón
+ * @license   https://github.com/SiroDiaz/DataStructures/blob/master/README.md (MIT License)
+ */
+namespace DataStructures\Exceptions;
+
+use Exception;
+
+/**
+ * NotFoundException
+ *
+ * NotFoundException is thrown when an element is not found, for example,
+ * in a list.
+ *
+ * @author Siro Diaz Palazon <siro_diaz@yahoo.com>
+ */
+class NotFoundException extends Exception {}
