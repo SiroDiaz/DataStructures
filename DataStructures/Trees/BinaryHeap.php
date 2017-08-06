5246c1f671c60fd52055720339d791751cb4ee8a
Siro Díaz Palazón <sirodiaz93@gmail.com>
2017-08-06 16:32:39 +0200

    Added binary heap class

---
+++ DataStructures/Trees/BinaryHeap.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * DataStructures for PHP
+ *
+ * @link      https://github.com/SiroDiaz/DataStructures
+ * @copyright Copyright (c) 2017 Siro Díaz Palazón
+ * @license   https://github.com/SiroDiaz/DataStructures/blob/master/README.md (MIT License)
+ */
+namespace DataStructures\Trees;
+
+/**
+ * BinaryHeap
+ * 
+ * Binary heap is a binary tree that is a complete tree making possible
+ * store it in an array. There are two types: minimum and maximum heap that are similar.
+ * It is used, for example, for algorithms like Dijkstra shortest path and Prim's minimum
+ * spanning tree.
+ *
+ * @author Siro Diaz Palazon <siro_diaz@yahoo.com>
+ */
+class BinaryHeap {
+
+}
