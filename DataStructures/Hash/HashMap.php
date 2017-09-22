34400b37e1ab7811eb5514b4e7f28c585fa1757e
Siro Diaz Palazon <sirodiaz93@gmail.com>
2017-09-22 12:00:30 +0200

    Implemented all methods in separate chaining hash table

--- DataStructures/Hash/HashMap.php
+++ DataStructures/Hash/HashMap.php
@@ -18,17 +18,24 @@ use Countable;
  * feature a fast search, insertion and deletion in O(1) (when n/B is near to 0),
  * but when it increases its size operations take O(1+(n/B)) when 1 is the hash calculation
  * and n/B is the number of elements stored(n) in the hashmap divided by B (buckets).
+ * 
+ * Memory usage since it is an array pointing (not pointers i know, but understand it if you use languages
+ * like C or C++) to an AVL tree it will take B * k1 + n * (2 * k1 + k2).
  */
 class HashMap implements HashTableInterface, Countable {
     private $size;
     private $count;
     private $loadFactor;
     private $resize;
+    private $defaultSize;
     private $hashTable;
 
     public function __construct($size=100, $loadFactor=0.75, $resize=2) {
         if($size <= 0) {
-            throw new OutOfBoundsException('initial size must be greater than 0');
+            throw new OutOfBoundsException('initial size must be greater than 0.');
+        }
+        if(is_float($size)) {
+            throw new InvalidArgumentException('Size must be an integer.');
         }
         if($loadFactor <= 0 || $loadFactor > 1) {
             throw new OutOfBoundsException('Load factor must be 0 > loadFactor <= 1. Not recommended 1.');
@@ -36,9 +43,13 @@ class HashMap implements HashTableInterface, Countable {
         if($resize <= 1) {
             throw new OutOfBoundsException('resize factor must be greater than 1.');
         }
+        if(is_float($resize)) {
+            throw new InvalidArgumentException('resize must be an integer.');
+        }
 
         $this->loadFactor = $loadFactor;
         $this->resize = $resize;
+        $this->defaultSize = $size;
         $this->size = $size;
         $this->count = 0;
         $this->hashTable = array_fill(0, $this->size, new AVLTree());
@@ -56,11 +67,15 @@ class HashMap implements HashTableInterface, Countable {
         return $this->count;
     }
 
-    public function getHash($key) {
+    private function needsResize() {
+        return $this->count >= $this->size * $this->loadFactor;
+    }
+
+    private function getHash($key) {
         $hash = 5381;
 
-        foreach($key as $k) {
-            $hash = (($hash << 5) + $hash) + $k;
+        for($i = 0; $i < mb_strlen($key); $i++) {
+            $hash = (($hash << 5) + $hash) + ord(mb_substr($key, $i, 1));
         }
 
         return $hash;
@@ -71,23 +86,56 @@ class HashMap implements HashTableInterface, Countable {
     }
 
     public function search($key) {
+        $bucket = $this->getBucket($key);
+        return $this->hashTable[$bucket]->get($key);
+    }
 
+    public function contains($key) : bool {
+        $bucket = $this->getBucket($key);
+        return $this->hashTable[$bucket]->exists($key);
     }
 
     public function insert($key, $val) {
-
+        $bucket = $this->getBucket($key);
+        $this->hashTable[$bucket]->put($key, $val, true);
+        $this->count++;
+        if($this->needsResize()) {
+            $this->resize();
+        }
     }
 
     public function delete($key) {
+        $bucket = $this->getBucket($key);
+        $deletedNode = $this->hashTable[$bucket]->delete($key);
+        if($deletedNode !== null) {
+            $this->count--;
+            return $deletedNode->data;
+        }
 
+        return null;
     }
 
     public function clear() {
-
+        $this->hashTable = [];
+        $this->size = $this->defaultSize;
+        $this->count = 0;
+        $this->hashTable = array_fill(0, $this->size, new AVLTree());
     }
 
     private function resize() {
+        $oldSize = $this->size;
+        $this->size = $this->size * $this->resize;
+        $oldTable = $this->hashTable;
+        $this->hashTable = array_fill(0, $this->size, new AVLTree());
+        for($bucket = 0; $bucket < $oldSize; $bucket++) {
+            $tree = $oldTable[$bucket];
+            $tree->preorder(function($node) {
+                $newBucket = $this->getBucket($node->key);
+                $this->hashTable[$newBucket]->put($node->key, $node->data);
+            });
+        }
 
+        unset($oldTable);
     }
 
     public function count() {
--- tests/Hash/HashMapTest.php
+++ tests/Hash/HashMapTest.php
@@ -38,14 +38,57 @@ class HashMapTest extends TestCase {
     }
         
     public function testInsert() {
-        $this->hashmap = new HashMap(5);
+        $this->hashmap = new HashMap(2);
+        $this->hashmap->insert('a', 'All');
+        $this->hashmap->insert('ax', 'Axon');
+        $this->assertEquals($this->hashmap->getSize(), 4);
+        $this->hashmap->insert('b', 'Before');
+        $this->assertEquals($this->hashmap->getSize(), 8);
+        $this->hashmap->insert('c', 'Class');
+        $this->hashmap->insert('d', 'Dig');
+        $this->hashmap->insert('hello_world', 'Hello World!');
+        $this->assertEquals($this->hashmap->getSize(), 16);
+        $this->assertEquals($this->hashmap->search('a'), 'All');
+        $this->assertEquals($this->hashmap->search('ax'), 'Axon');
+        $this->assertEquals($this->hashmap->search('b'), 'Before');
+        $this->assertEquals($this->hashmap->search('c'), 'Class');
+        $this->assertEquals($this->hashmap->search('d'), 'Dig');
+        $this->assertEquals($this->hashmap->search('hello_world'), 'Hello World!');
     }
         
     public function testDelete() {
         $this->hashmap = new HashMap(5);
+        $this->hashmap->insert('a', 'All');
+        $this->hashmap->insert('ax', 'Axon');
+        $this->hashmap->insert('b', 'Before');
+        $this->hashmap->insert('c', 'Class');
+        $this->hashmap->insert('d', 'Dig');
+        $this->hashmap->insert('hello_world', 'Hello World!');
+        $this->assertEquals($this->hashmap->delete('d'), 'Dig');
+        $this->assertEquals($this->hashmap->getCount(), 5);
+        $this->assertEquals($this->hashmap->delete('c'), 'Class');
+        $this->assertEquals($this->hashmap->getCount(), 4);
+
+        $this->assertEquals($this->hashmap->delete('b'), 'Before');
+        $this->assertEquals($this->hashmap->getCount(), 3);
+        $this->assertEquals($this->hashmap->delete('ax'), 'Axon');
+        $this->assertEquals($this->hashmap->getCount(), 2);
+        $this->assertEquals($this->hashmap->delete('a'), 'All');
+        $this->assertEquals($this->hashmap->getCount(), 1);
+        $this->assertEquals($this->hashmap->delete('hello_world'), 'Hello World!');
+        $this->assertEquals($this->hashmap->getCount(), 0);
     }
         
     public function testClear() {
         $this->hashmap = new HashMap(5);
+        $this->hashmap->insert('a', 'All');
+        $this->hashmap->insert('ax', 'Axon');
+        $this->hashmap->insert('b', 'Before');
+        $this->hashmap->insert('c', 'Class');
+        $this->hashmap->insert('d', 'Dig');
+        $this->hashmap->insert('hello_world', 'Hello World!');
+        $this->hashmap->clear();
+        $this->assertEquals($this->hashmap->getCount(), 0);
+        $this->assertEquals($this->hashmap->getSize(), 5);
     }
 }
